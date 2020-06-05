3059328ed2efe1d73814d75b987253dbc414c838
Aditya Malhotra <adityamalhotramcs@gmail.com>
2020-06-05 14:51:59 +0530

    uploaded scraper

---
+++ try1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 13:06:26 2020
+
+@author: Aditya
+"""
+
+import glassdoor_data as gs
+import pandas as pd
+path = "C:/Users/Aditya/Documents/salary_prediction/chromedriver"
+df = gs.get_jobs('data scientist',15,False,path,15)
+
